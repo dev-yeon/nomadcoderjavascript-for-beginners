8698db63d4ba50692fedb7e608214b70f7fbcb30
contest0717 <contest0717@gmail.com>
2024-06-12 23:48:01 +0900

    first

---
+++ app.js
@@ -0,0 +1,21 @@
+//integer 
+//float 
+
+//constant 인 variable을 만듦, 
+//const a의 값만  바꾸면 되니까 코드 변경이 쉬워짐 
+const a = 5;
+const b = 2;
+const myName = "yeon"; // Js는 camel 케이스를 사용 
+const veryVeryVeryLongName = "Yeooooooooooooooon";
+
+console.log(544444);
+
+
+console.log( a + b );
+console.log( a * b );
+console.log( a / b );
+
+// variable 는 값을 저장하거나 유지하는 역할
+
+
+console.log("Hello, " + veryVeryVeryLongName); 
