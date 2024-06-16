24359578c58713f6aae210f52ebad805a9cc3d56
contest0717 <contest0717@gmail.com>
2024-06-17 00:52:14 +0900

    240617_ 바닐라 JS로 크롬 앱 만들기
    #4.3 Events part Two (08:08)

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-// const loginForm = document.getElementById("login-form");
 
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-
-function onLoginBtnClick() {
-    // console.dir(loginInput);
-    // console.log(loginInput.value);
-    // console.log("Click!!!!!!");
-
-    const username = loginInput.value;
-    console.log(username);
-    // if(username === "") {
-    //     alert("Please write your name."); 
-
-    // } else if(username.length) {
-    //     alert("Your name is too long.")
-    // }
+function onLoginSubmit(tomato) {
+    tomato.preventDefault();  
+    console.log(tomato);
 }
 
-
-loginButton.addEventListener("click", onLoginBtnClick)
+loginForm.addEventListener("submit", onLoginSubmit);
