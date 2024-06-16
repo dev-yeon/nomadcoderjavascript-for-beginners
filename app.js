27f1d42c56a3c9b485a090ce8b995ade3d3ba022
contest0717 <contest0717@gmail.com>
2024-06-17 02:05:37 +0900

    [FE][DEV] getting username
    - hidden

--- app.js
+++ app.js
@@ -2,18 +2,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-const link = document.querySelector("a");
+
+
 function onLoginSubmit(event) {
-    tomato.preventDefault();  
-    console.log(loginInput.value);
+    event.preventDefault();  
+    const username = loginInput.value;
+    loginForm.classList.add("hidden");
+    console.log(username);
+
 }
 
-function handleLinkClick(event) {
-    // alert("Clicked!")
-    event.preventDefault();
-    console.dir(event);
-    
-}
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
